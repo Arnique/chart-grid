87760fcb3a443445d44a0c6589f70b2a86d1e81f
arnold <arnold@arnique.net>
2025-08-14 13:13:20 +0300

    add grid-switcher component

---
+++ app/components/GridSwitcher.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center gap-2">
+    <UButton size="sm" color="primary" variant="outline">1x4</UButton>
+    <UButton size="sm" color="neutral" variant="outline">4x1</UButton>
+    <UButton size="sm" color="neutral" variant="outline">2x2</UButton>
+  </div>
+</template>
