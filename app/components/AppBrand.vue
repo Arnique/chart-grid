f1641323da179fbc3981dc44e064a2ece6181703
arnold <arnold@arnique.net>
2025-08-14 12:52:04 +0300

    add default layout

---
+++ app/components/AppBrand.vue
@@ -0,0 +1,6 @@
+<template>
+  <NuxtLink to="/" class="flex gap-2 items-center text-lg">
+    <UIcon name="flat-color-icons:combo-chart"/>
+    <span class="font-bold">Chart<span class="text-primary">Grid</span></span>
+  </NuxtLink>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,10 @@
 <template>
   <UApp>
-    <main>
-      <slot></slot>
-    </main>
+    <div class="relative lg:h-screen lg:w-screen lg:flex lg:flex-col">
+      <AppHeader />
+      <main class="lg:flex-1 bg-white/5">
+        <slot></slot>
+      </main>
+    </div>
   </UApp>
 </template>
