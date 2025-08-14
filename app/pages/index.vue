7118cbc33f0d926c63475d92f9c01cd27d975d55
arnold <arnold@arnique.net>
2025-08-14 13:00:15 +0300

    add home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-<h1>Hello World!</h1>
+<section class="p-[10px] gap-[10px] w-full h-full relative grid grid-cols-2 grid-rows-2">
+  <div class="bg-gray-900"></div>
+  <div class="bg-gray-900"></div>
+  <div class="bg-gray-900"></div>
+  <div class="bg-gray-900"></div>
+</section>
 </template>
 
 <script setup>
